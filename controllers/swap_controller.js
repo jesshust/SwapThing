16eae9d7a7ac3a8695b11375789de2ed2a04f7c4
jess hust <jessicakhust@gmail.com>
2016-08-09 21:52:43 -0500

    sha1 authentication

--- controllers/swap_controller.js
+++ controllers/swap_controller.js
@@ -3,14 +3,7 @@ var router = express.Router();
 var bodyParser = require('body-parser'); 
 var methodOverride = require('method-override'); 
 var models = require('../models'); 
-
-// var app = express();
-// app.use(require('serve-static')(__dirname + '/../../public'));
-// app.use(require('cookie-parser')());
-// app.use(require('body-parser').urlencoded({ extended: true }));
-// app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+var sha1 = require('sha1'); 
 
 
 router.get('/', function (req, res){
@@ -78,17 +71,5 @@ router.get('/userLanding', function (req, res){
 // 	});
 // });
 
-
-var passport = require('passport'); 
-var Strategy = require('passport-local').Strategy; 
-
-passport.use(new Strategy(function(username, password, cb) {
-	users.users.findByUsername(username, function(err, user) {
-		if(err) {return cb(err); }
-		if(!user) {return cb(null, false); }
-		if(user.password != password){ return cb(null, false); }
-		return cb(null, user); 
-	}); 
-})); 
 
 module.exports = router;
